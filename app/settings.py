69b9694a260e353be343e6928671bf1dfe88d24e
Bitboy7 <luisganerx@gmail.com>
2025-07-19 19:43:52 -0600

    fix: update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS to use DOMAIN environment variable

--- app/settings.py
+++ app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1", "yes"]
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("DOMAIN")
 
 # Configuración de seguridad para producción
 if not DEBUG:
@@ -47,7 +47,7 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = os.getenv("CSRF_COOKIE_SECURE", "False").lower() in ["true", "1"]
     X_FRAME_OPTIONS = 'DENY'
 
-CRSF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DOMAIN")
 
 # Application definition
 
