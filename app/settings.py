092c27ef8f08a8d499ee01a3b32334521125b5dd
Bitboy7 <luisganerx@gmail.com>
2025-07-21 02:13:38 -0600

    feat: add gettext setup script and disable language settings temporarily

--- app/settings.py
+++ app/settings.py
@@ -90,53 +90,53 @@ UNFOLD = {
     "SHOW_HISTORY": True,
     "SHOW_VIEW_ON_SITE": True,
     "SHOW_BACK_BUTTON": True,
-    "SHOW_LANGUAGES": True,  # Habilitar selector de idiomas
+    # "SHOW_LANGUAGES": True,  # Habilitar selector de idiomas - Temporalmente deshabilitado
     "ENVIRONMENT": "app.environment_callback",
     "DASHBOARD_CALLBACK": "app.views.dashboard_callback",
     "THEME": None,  # Permite cambio de tema
     "LOGIN": {
         "image": lambda request: static("img/login-bg.png") if static("img/login-bg.png") else None,
     },
-    # Configuración personalizada de idiomas
-    "LANGUAGES": {
-        "navigation": [
-            {
-                'bidi': False,
-                'code': 'es',
-                'name': 'Spanish',
-                'name_local': 'Español',
-                'name_translated': 'Español'
-            },
-            {
-                'bidi': False,
-                'code': 'en',
-                'name': 'English',
-                'name_local': 'English',
-                'name_translated': 'Inglés'
-            },
-            {
-                'bidi': False,
-                'code': 'fr',
-                'name': 'French',
-                'name_local': 'Français',
-                'name_translated': 'Francés'
-            },
-            {
-                'bidi': False,
-                'code': 'de',
-                'name': 'German',
-                'name_local': 'Deutsch',
-                'name_translated': 'Alemán'
-            },
-            {
-                'bidi': False,
-                'code': 'pt',
-                'name': 'Portuguese',
-                'name_local': 'Português',
-                'name_translated': 'Portugués'
-            },
-        ],
-    },
+    # Configuración personalizada de idiomas - Temporalmente deshabilitado
+    # "LANGUAGES": {
+    #     "navigation": [
+    #         {
+    #             'bidi': False,
+    #             'code': 'es',
+    #             'name': 'Spanish',
+    #             'name_local': 'Español',
+    #             'name_translated': 'Español'
+    #         },
+    #         {
+    #             'bidi': False,
+    #             'code': 'en',
+    #             'name': 'English',
+    #             'name_local': 'English',
+    #             'name_translated': 'Inglés'
+    #         },
+    #         {
+    #             'bidi': False,
+    #             'code': 'fr',
+    #             'name': 'French',
+    #             'name_local': 'Français',
+    #             'name_translated': 'Francés'
+    #         },
+    #         {
+    #             'bidi': False,
+    #             'code': 'de',
+    #             'name': 'German',
+    #             'name_local': 'Deutsch',
+    #             'name_translated': 'Alemán'
+    #         },
+    #         {
+    #             'bidi': False,
+    #             'code': 'pt',
+    #             'name': 'Portuguese',
+    #             'name_local': 'Português',
+    #             'name_translated': 'Portugués'
+    #         },
+    #     ],
+    # },
     "COLORS": {
         "base": {
             "50": "248, 250, 252",   # Gris muy claro
@@ -444,24 +444,24 @@ LANGUAGE_CODE = "es"  # Idioma por defecto
 
 TIME_ZONE = "America/Mexico_City"
 
-USE_I18N = True  # Habilitar internacionalización
+USE_I18N = False  # Deshabilitar internacionalización temporalmente
 
 # Desactivamos el soporte de zona horaria para evitar errores con MySQL
 USE_TZ = False
 
-# Idiomas disponibles en la aplicación
-LANGUAGES = (
-    ("es", _("Español")),
-    ("en", _("English")),
-    ("fr", _("Français")),
-    ("de", _("Deutsch")),
-    ("pt", _("Português")),
-)
+# Idiomas disponibles en la aplicación - Temporalmente deshabilitado
+# LANGUAGES = (
+#     ("es", _("Español")),
+#     ("en", _("English")),
+#     ("fr", _("Français")),
+#     ("de", _("Deutsch")),
+#     ("pt", _("Português")),
+# )
 
-# Directorio donde se almacenan los archivos de traducción
-LOCALE_PATHS = [
-    BASE_DIR / 'locale',
-]
+# Directorio donde se almacenan los archivos de traducción - Temporalmente deshabilitado
+# LOCALE_PATHS = [
+#     BASE_DIR / 'locale',
+# ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
